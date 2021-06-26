d7b061516d01b2f78dc6d3d7a91b783a2e54f775
Mario Mancilla <mariormt17@gmail.com>
2021-06-26 00:02:18 -0500

    Change color in navbar items when is responsive

--- js/index.js
+++ js/index.js
@@ -1,10 +1,13 @@
+menu = document.getElementById("header");
+navbar = document.getElementById("navbar");
+header = document.getElementById("header");
+listItem1 = document.getElementById("listItem1");
+listItem2 = document.getElementById("listItem2");
+listItem3 = document.getElementById("listItem3");
+
 // FUNCTION TO ADD NEW STYLE TO NAVBAR WHEN THE SCROLL DOWN
 window.onscroll = function () {
   scroll = document.documentElement.scrollTop;
-  header = document.getElementById("header");
-  listItem1 = document.getElementById("listItem1");
-  listItem2 = document.getElementById("listItem2");
-  listItem3 = document.getElementById("listItem3");
   if (scroll > 20) {
     header.classList.add('nav-mod');
     listItem1.classList.add("list-item-black");
@@ -18,13 +21,13 @@ window.onscroll = function () {
   }
 }
 
-menu = document.getElementById("header");
-navbar = document.getElementById("navbar");
-
 // FUNCTION TO SHOW THE NAVBAR WHEN THE HAMBURGER BUTTON WAS CLICKED
 document.getElementById("btn-header-responsive").addEventListener("click", function () {
   menu.classList.toggle("move-content");
   navbar.classList.toggle("move-navbar");
+  listItem1.classList.toggle("list-item-black");
+  listItem2.classList.toggle("list-item-black");
+  listItem3.classList.toggle("list-item-black");
 })
 
 // FUNCTION TO RESTORE THE NAVBAR WHEN THE WINDOW HAS A WIDTH MAJOR THAN 760
